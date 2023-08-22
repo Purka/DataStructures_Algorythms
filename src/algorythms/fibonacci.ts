ed9b0bf39bde4881138340e7af97ffaef92fb31c
Gromova <malyarchyk.17@gmail.com>
2023-08-22 18:04:39 +0200

    add iterative fibonacci solution

--- src/algorythms/fibonacci.ts
+++ src/algorythms/fibonacci.ts
@@ -14,3 +14,12 @@ function fibonacciRecursive(n: number): number {
 }
 
 console.log(fibonacciRecursive(8))
+
+function fibonacciIterative(n: number) {
+    let arr = [0, 1];
+    for (let i = 2; i < n + 1; i++) {
+        arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr[n];
+}
+fibonacciIterative(3);
